a0c50f7d48fec54713c070ceaa7ee945d48cf297
Build Server <automation@CinchShare.com>
2022-11-30 04:23:15 +0000

    Add project files.

---
+++ ReservationBackend.API/Controllers/ScheduleController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReservationBackend.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReservationBackend.API.Models;
+namespace ReservationBackend.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScheduleController : ControllerBase
+    {
+      
+
+        private readonly ILogger<ScheduleController> _logger;
+        private readonly IScheduleService _scheduleService;
+
+        public ScheduleController(ILogger<ScheduleController> logger, IScheduleService scheduleService)
+        {
+            _logger = logger;
+            _scheduleService = scheduleService;
+        }
+
+
+        [HttpPost]
+        public ActionResult AddSchedule([FromBody] Schedule schedule)
+        {
+
+            return Ok(_scheduleService.AddSchedule(schedule.ProviderId, schedule.StartTime, schedule.EndTime));
+        }
+
+
+        [HttpGet]
+        public ActionResult ListFreeTimeSlots()
+        {
+
+            return Ok(_scheduleService.ListFreeTimeSlots());
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult ReserveTimeSlot(int id)
+        {
+            _scheduleService.ReserveTimeSlot(id);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult ConfirmTimeSlot(int id)
+        {
+            _scheduleService.ConfirmTimeSlot(id);
+            return Ok();
+        }
+    }
+}
---
+++ ReservationBackend/DTO/ScheduleDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReservationBackend.DTO
+{
+    public class ScheduleDTO
+    {
+        public int ProviderId { get; set; }
+
+        public string ProviderName { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public bool IsReserved { get; set; }
+
+        public bool IsConfirmed { get; set; }
+    }
+}
